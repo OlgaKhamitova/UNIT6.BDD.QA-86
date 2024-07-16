39a3ec146d289a4a468434276fbaac6c5ef1ef88
Olga Khamitova <olgacherkasova13317@rambler.ru>
2024-07-16 14:38:44 +0300

    LoginPage1 is added

---
+++ src/test/java/ru/netology/page/LoginPage1.java
@@ -0,0 +1,13 @@
+package ru.netology.page;
+
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class LoginPage1 {
+    public void validLogin(DataHelper.AuthInfo info){
+        $("[data-test-id=login] input").setValue(info.getLogin());
+        $("[data-test-id=password] input").setValue(info.getPassword());
+        $("[data-test-id=action-login]").click();
+    }
+}
